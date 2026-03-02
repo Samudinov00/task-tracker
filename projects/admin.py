5e93f2dad8385638234b4b54c755d586f7ad5e9e
Илим Самудинов <Silim@stat.kg>
2026-03-02 17:13:43 +0600

    ограничить выбор ролей в админке: клиенты только клиенты, исполнители только исполнители

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Project, Task, Comment, Notification
+from accounts.models import CustomUser
 
 
 @admin.register(Project)
@@ -8,12 +9,34 @@ class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     filter_horizontal = ('executors', 'clients')
 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'executors':
+            kwargs['queryset'] = CustomUser.objects.filter(role='executor')
+        elif db_field.name == 'clients':
+            kwargs['queryset'] = CustomUser.objects.filter(role='client')
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'manager':
+            kwargs['queryset'] = CustomUser.objects.filter(role='manager')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'project', 'status', 'assignee', 'deadline')
     list_filter  = ('status', 'project')
     search_fields = ('title',)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'assignee':
+            kwargs['queryset'] = CustomUser.objects.filter(role='executor')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'clients':
+            kwargs['queryset'] = CustomUser.objects.filter(role='client')
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
 
 
 admin.site.register(Comment)
